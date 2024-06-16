5eb9fe4557a98cbdfa56e097735517a1c712b97f
florinres <raresbolca@gmail.com>
2024-06-16 18:19:06 +0300

    masinute si logica camera

--- main.cpp
+++ main.cpp
@@ -2,14 +2,35 @@
 #include <cmath>
 #include <Windows.h>
 #include "glut.h"
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+#include "glut.h"
 
-double angleHorizontal = 0.0;  // Unghiul pentru rotația camerei orizontală
-double angleVertical = 0.0;    // Unghiul pentru rotația camerei verticală
-double radius = 10.0;          // Raza cercului pe care se mișcă camera, setată mai mare pentru a muta camera mai departe de obiect
+double angleHorizontal = 0.0; 
+double departareVerticala = 0.0;    
+double radius = 10.0;          
+double departareZ = 5.0;
+
+void animatieMasina(int value) {
+    while (--value)
+    {
+        
+    }
+    std::cout << "called";
+}
+
+void roata(double x, double y, double z) {
+    glPushMatrix(); // Salvează starea curentă a matricei
+    glTranslated(x, y, z); // Translatează (mută) roata la poziția specificată
+    glRotated(90, 0, 1, 0); // Rotește roata astfel încât să fie perpendiculară pe axa X
+    glColor3d(0.1, 0.1, 0.1); // Setează culoarea roții
+    glutSolidTorus(0.1, 0.3, 12, 36); // Desenează torusul cu raza interioară 0.1 și raza exterioară 0.3
+    glPopMatrix(); // Restabilește starea anterioară a matricei
+}
 
 void masina() {
     glColor3d(1.0, 0.0, 0.0);
-    glTranslated(-2.0, -0.5, 0.0);
     glBegin(GL_QUADS);
     {//bumper
         glVertex3d(0.0, 0.0, 0.0);
@@ -40,15 +61,6 @@ void masina() {
     glBegin(GL_QUADS);
     {//bumper spate
         glVertex3d(0.0, 0.0, -3.0);
-        glVertex3d(0.0, 0.0, -3.0);
-        glVertex3d(0.0, 0.5, -3.0);
-        glVertex3d(0.0, 0.5, 0.0);
-    }
-    glEnd();
-
-    glBegin(GL_QUADS);
-    {//bumper spate
-        glVertex3d(0.0, 0.0, -3.0);
         glVertex3d(3.0, 0.0, -3.0);
         glVertex3d(3.0, 0.5, -3.0);
         glVertex3d(0.0, 0.5, -3.0);
@@ -56,7 +68,7 @@ void masina() {
     glEnd();
 
     glColor3d(1.0, 1.0, 0.0);
-    glBegin(GL_QUADS); 
+    glBegin(GL_QUADS);
     {//far fata
         glVertex3d(0.1, 0.15, 0.01);
         glVertex3d(0.3, 0.15, 0.01);
@@ -89,12 +101,61 @@ void masina() {
     }
     glEnd();
 
+    glColor3d(0.0, 1.0, 0.0);
+    glBegin(GL_QUADS);
+    {
+        // Partea de sus a capotei
+        glVertex3d(0.0, 0.5, 0.0);
+        glVertex3d(3.0, 0.5, 0.0);
+        glVertex3d(3.0, 0.65, -0.8);
+        glVertex3d(0.0, 0.65, -0.8);
+
+        // Fața laterală dreapta
+        glVertex3d(3.0, 0.5, 0.0);
+        glVertex3d(3.0, 0.65, -0.8);
+        glVertex3d(3.0, 0.5, -0.8);
+        glVertex3d(3.0, 0.5, 0.0);
+
+        // Fața laterală stânga
+        glVertex3d(0.0, 0.5, 0.0);
+        glVertex3d(0.0, 0.65, -0.8);
+        glVertex3d(0.0, 0.5, -0.8);
+        glVertex3d(0.0, 0.5, 0.0);
+
+        // Fața frontală (care îmbină capota cu partea din față a mașinii)
+        glVertex3d(0.0, 0.5, -0.8);
+        glVertex3d(3.0, 0.5, -0.8);
+        glVertex3d(3.0, 0.65, -0.8);
+        glVertex3d(0.0, 0.65, -0.8);
+    }
+    glEnd();
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+    {
+        glVertex3d(0.0, 0.0, 0.0);
+        glVertex3d(0.0, 0.0, -3.0);
+        glVertex3d(3.0, 0.0, -3.0);
+        glVertex3d(3.0, 0.0, 0.0);
+    }
+    glEnd();
+
+    roata(-0.10, 0.1, -0.7);
+    roata(3.1, 0.1, -0.7);
+    roata(-0.10, 0.1, -2.50);
+    roata(3.1, 0.1, -2.50);
 }
 
 void Taste(unsigned char key, int x, int y) {
-    std::cout << key;
+    std::cout << "tasta " << key << std::endl;
     switch (key) {
     case 27: exit(1);
+        break;
+    case 'w':
+        departareZ += 0.25;
+        break;
+    case 's':
+        departareZ -= 0.25;
         break;
     default:
         break;
@@ -103,7 +164,7 @@ void Taste(unsigned char key, int x, int y) {
 }
 
 void TasteSpeciale(int key, int x, int y) {
-    std::cout << key;
+    std::cout << "tasta " << key << std::endl;
     switch (key) {
     case GLUT_KEY_LEFT:
         angleHorizontal -= 0.05; // Ajustăm unghiul camerei orizontal
@@ -112,10 +173,10 @@ void TasteSpeciale(int key, int x, int y) {
         angleHorizontal += 0.05; // Ajustăm unghiul camerei orizontal
         break;
     case GLUT_KEY_UP:
-        angleVertical -= 0.05; // Ajustăm unghiul camerei vertical
+        departareVerticala -= 0.5; // Ajustăm unghiul camerei vertical
         break;
     case GLUT_KEY_DOWN:
-        angleVertical += 0.05; // Ajustăm unghiul camerei vertical
+        departareVerticala += 0.5; // Ajustăm unghiul camerei vertical
         break;
     default:
         break;
@@ -142,15 +203,21 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    double cameraX = radius * cos(angleVertical) * cos(angleHorizontal);
-    double cameraY = radius * sin(angleVertical);
-    double cameraZ = radius * cos(angleVertical) * sin(angleHorizontal);
+    double cameraX = radius * cos(departareVerticala) * sin(angleHorizontal);
+    double cameraY = radius * sin(departareVerticala);
+    double cameraZ = radius * cos(departareVerticala) * cos(angleHorizontal);
 
-    gluLookAt(cameraX, cameraY, cameraZ,  // Poziția camerei
-        -2.0, -0.5, 0.0,   // Punctul la care privește
+    gluLookAt(cameraX, cameraY, cameraZ-departareZ,   // Poziția camerei
+        0.0, 0.0, 0.0,   // Punctul la care privește
         0.0, 1.0, 0.0);  // Vectorul de sus
 
+    glTranslated(-2.0, 0.1, 0.0);
     masina();
+    //glPushMatrix();
+    glRotated(180, 0, 1, 0);
+    glTranslatef(2.0, 0.1, 0.0);
+    masina();
+    //glPopMatrix();
 
     glutSwapBuffers();
 }
@@ -167,6 +234,7 @@ int main(int argc, char** argv) {
     glutDisplayFunc(display);
     glutSpecialFunc(TasteSpeciale);
     glutKeyboardFunc(Taste);
+    glutTimerFunc(100, animatieMasina, 1);
 
     glutMainLoop();
 
